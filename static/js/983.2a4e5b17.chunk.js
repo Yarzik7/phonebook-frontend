985b9b8e694b232803ece48f75c77ddae35e63c4
Yarzik7 <116737021+Yarzik7@users.noreply.github.com>
2023-06-26 14:08:41 +0000

    Deploying to gh-pages from @ Yarzik7/goit-react-hw-08-phonebook@9796e1c7e2183f19e4c88861aafbd6bbe99e9a49 🚀

---
+++ static/js/983.2a4e5b17.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,n){n.r(t);var i=n(184),c={container:{minHeight:"calc(100vh - 91px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Welcome to PHONEBOOK!"})})}}}]);
+//# sourceMappingURL=983.2a4e5b17.chunk.js.map
